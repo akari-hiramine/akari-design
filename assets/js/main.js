02dcb2b721ae7319eec5512093320486c62c3ca4
akari-hiramine <akagaju.1230@gmail.com>
2025-02-17 15:08:44 +0900

    2/17時点での最新版をアップロードした。

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 
-window.addEventListener('scroll', () => {
-  const scrollValue = document.scrollingElement.scrollTop;
+// window.addEventListener('scroll', () => {
+//   const scrollValue = document.scrollingElement.scrollTop;
 
-  if (scrollValue >= 300) {
-    header.style.display = 'flex';
-  } else{
-    header.style.display = 'none';
+//   if (scrollValue >= 200) {
+//     header.style.display = 'flex';
+//   } else{
+//     header.style.display = 'none';
+//   }
+
+// });
+
+
+$(function(){
+ //  リンクホバー時アニメーション
+ $('#header-logo a, #nav-pc a, #works-container img, #bt-detail, #footer-link a, #bt-pagetop-pc, .bt-back').hover(
+  function(){
+    $(this).animate({
+      opacity: 0.5,
+    }, 300);  
+  },
+  function(){
+    $(this).animate({
+      opacity: 1.0,
+    }, 300);
   }
+ );
 
 });
